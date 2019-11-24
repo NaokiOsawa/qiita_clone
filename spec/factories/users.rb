9258564f7dda7c3e3bb071e1b84b81a5c35ed000
大沢直樹 <>
2019-11-24 11:49:15 +0900

    user modelのfactoryの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    account { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 8) }
   end
 end
