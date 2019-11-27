4b7519eeb00d14a72306846e40c42a9c841e498f
大沢直樹 <>
2019-11-27 15:13:32 +0900

    user modelのfactoryの設定(passwordの最低文字数を6文字に修正)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     account { Faker::Internet.username }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 8) }
+    password { Faker::Internet.password(min_length: 6) }
   end
 end
