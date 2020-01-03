207b71b179a4c46c6034357b6753bc7fb27a27c4
大沢直樹 <>
2020-01-03 22:56:03 +0900

    config/routes.rbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'auth/registrations'
+      }
       resources :articles
     end
   end
