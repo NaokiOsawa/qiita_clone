ae240141c564e15618b71eb1cacb4494e07b3cc2
大沢直樹 <>
2020-01-04 19:52:44 +0900

    指摘事項の修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  private
 
-    def sign_up_params
-      params.permit(:name, :email, :account, :password, :password_confirmation)
-    end
+  def sign_up_params
+    params.permit(:name, :email, :account, :password)
+  end
 
-    def account_update_params
-      params.permit(:name, :email, :team)
-    end
+  def account_update_params
+    params.permit(:name, :email)
+  end
 end
