8835e0a6a9f351d38dedd4c0a4d455ec59d97018
大沢直樹 <>
2020-01-04 16:53:22 +0900

    ストロングパラメーターの修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
     def sign_up_params
-      params.permit(:name, :email, :team, :password, :password_confirmation)
+      params.permit(:name, :email, :account, :password, :password_confirmation)
     end
 
     def account_update_params
