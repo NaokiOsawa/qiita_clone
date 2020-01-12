b59c5b09dca35395727771d29c8c868dac62dedf
大沢直樹 <>
2020-01-12 14:23:27 +0900

    指摘事項の修正

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -4,15 +4,62 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
   describe "POST /api/v1/auth" do
     subject { post(api_v1_user_registration_path, params: params) }
 
-    let(:params) { attributes_for(:user) }
+    context "ユーザー登録に必要な情報が正しく送信された時" do
+      let(:params) { attributes_for(:user) }
 
-    it "ユーザー登録できる" do
-      subject
-      expect(response.headers["uid"]).to be_present
-      expect(response.headers["access-token"]).to be_present
-      expect(response.headers["client"]).to be_present
-      expect(response).to have_http_status(:ok)
+      it "ユーザー登録できる" do
+        expect { subject }.to change { User.count }.by(1)
+        expect(response.headers["uid"]).to be_present
+        expect(response.headers["access-token"]).to be_present
+        expect(response.headers["client"]).to be_present
+        expect(response).to have_http_status(:ok)
+      end
     end
+
+    context "ユーザー登録に必要な情報が正しく送信されない時(nameがnil)" do
+      let(:params) { attributes_for(:user,name:nil) }
+
+      it "ユーザー登録されない" do
+        expect { subject }.to change { User.count }.by(0)
+        expect(response.headers["uid"]).to be_blank
+        expect(response.headers["access-token"]).to be_blank
+        expect(response.headers["client"]).to be_blank
+      end
+    end
+
+    context "ユーザー登録に必要な情報が正しく送信されない時(accountがnil)" do
+      let(:params) { attributes_for(:user,account:nil) }
+
+      it "ユーザー登録されない" do
+        expect { subject }.to change { User.count }.by(0)
+        expect(response.headers["uid"]).to be_blank
+        expect(response.headers["access-token"]).to be_blank
+        expect(response.headers["client"]).to be_blank
+      end
+    end
+
+    context "ユーザー登録に必要な情報が正しく送信されない時(emailがnil)" do
+      let(:params) { attributes_for(:user,email:nil) }
+
+      it "ユーザー登録されない" do
+        expect { subject }.to change { User.count }.by(0)
+        expect(response.headers["uid"]).to be_blank
+        expect(response.headers["access-token"]).to be_blank
+        expect(response.headers["client"]).to be_blank
+      end
+    end
+
+    context "ユーザー登録に必要な情報が正しく送信されない時(passwordがnil)" do
+      let(:params) { attributes_for(:user,password:nil) }
+
+      it "ユーザー登録されない" do
+        expect { subject }.to change { User.count }.by(0)
+        expect(response.headers["uid"]).to be_blank
+        expect(response.headers["access-token"]).to be_blank
+        expect(response.headers["client"]).to be_blank
+      end
+    end
+
   end
 
   describe "POST /api/v1/auth/sign_in" do
@@ -63,6 +110,7 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
 
     it "ログアウトできる" do
       subject
+      expect(current_user.reload.tokens).to be_blank
       expect(response.headers["uid"]).to be_blank
       expect(response.headers["access-token"]).to be_blank
       expect(response.headers["client"]).to be_blank
