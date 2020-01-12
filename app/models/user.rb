f230e58cfe4c5077be524b3ed8c1bd6a26864241
大沢直樹 <>
2020-01-12 19:05:17 +0900

    name項目の必須入力のvalidatesをコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   validates :account, presence: true, uniqueness: true
-  validates :name, presence: true
+  #validates :name, presence: true
 
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
