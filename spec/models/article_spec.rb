c84b245df4d84168e5b492dd62fd77bbb66f7503
大沢直樹 <>
2020-01-13 21:55:18 +0900

    テスト実装

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,6 +8,20 @@ RSpec.describe Article, type: :model do
         expect(article).to be_valid
       end
     end
+    context "body,title,下書きが設定されている場合" do
+      let(:article) { build(:article, status: 0) }
+      it "下書き設定した記事が作られる" do
+        expect(article).to be_valid
+        expect(article.status).to eq "draft"
+      end
+    end
+    context "body,title,公開が設定されている場合" do
+      let(:article) { build(:article, status: 1) }
+      it "公開設定した記事が作られる" do
+        expect(article).to be_valid
+        expect(article.status).to eq "published"
+      end
+    end
   end
 
   describe "異常系のテスト" do
