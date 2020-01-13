c9e20e058457e4a3b4b0e8057381ea64a9b90dc8
大沢直樹 <>
2020-01-13 21:04:14 +0900

    modelにenumを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
+  enum status: { draft: 0, published: 1 }
+
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
