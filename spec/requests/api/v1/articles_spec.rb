30aca57f5360ac450ecd531dfd284d261273322c
大沢直樹 <>
2020-01-15 18:02:22 +0900

    テスト実装(途中)

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -5,15 +5,14 @@ RSpec.describe "Api::V1::Articles", type: :request do
     subject { get(api_v1_articles_path) }
 
     before do
-      create_list(:article, 3)
+      create_list(:article, 3,status:"published")
     end
 
-    it "記事一覧が取得できる" do
+    it "公開設定にしている記事一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
-      binding.pry
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id", "body", "title", "user"]
+      expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "status", "user"]
       expect(res[0]["user"].keys).to eq ["id", "account", "name"]
       expect(response).to have_http_status(:ok)
     end
@@ -23,18 +22,18 @@ RSpec.describe "Api::V1::Articles", type: :request do
     subject { get(api_v1_article_path(article_id)) }
 
     context "指定したidの記事が存在する場合" do
-      let(:article) { create(:article) }
+      let(:article) { create(:article, status:"published") }
       let(:article_id) { article.id }
 
       it "記事の値が取得できる" do
         subject
         res = JSON.parse(response.body)
         expect(response).to have_http_status(:ok)
-
         expect(res["id"]).to eq article.id
         expect(res["body"]).to eq article.body
         expect(res["title"]).to eq article.title
         expect(res["updated_at"]).to be_present
+        expect(res["status"]).to eq "published"
         expect(res["user"]["id"]).to eq article.user.id
         expect(res["user"]["account"]).to eq article.user.account
         expect(res["user"]["name"]).to eq article.user.name
