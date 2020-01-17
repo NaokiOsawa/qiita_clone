5edc0c3c8d33606e9f6c47234843d9c7ee6b0fa2
大沢直樹 <>
2020-01-17 12:42:43 +0900

    routes.rbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
         registrations: "api/v1/auth/registrations",
       }
       resources :articles
+      namespace :articles do
+        resources :draft, only: [:index, :show]
+      end
     end
   end
 end
