127450fb50cea33790894e758685b4b7283f67d1
大沢直樹 <>
2020-01-17 17:17:21 +0900

    draft_controller.rbの修正(current_userの要素追加)

--- app/controllers/api/v1/articles/draft_controller.rb
+++ app/controllers/api/v1/articles/draft_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::Articles::DraftController < Api::V1::ApiController
+  before_action :authenticate_user!, only: [:index, :show]
+
   def index
-    articles = Article.draft
+    articles = current_user.articles.draft
     render json: articles
   end
 
   def show
-    article = Article.draft.find(params[:id])
+    article = current_user.articles.draft.find(params[:id])
     render json: article
   end
 end
